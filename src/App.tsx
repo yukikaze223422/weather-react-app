039d96c2a83a61edcf1fbc669c736a36f9446fde
takagi <takagi09245@gmail.com>
2023-01-30 12:13:41 +0900

    日付データ追加・天気取得

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 import {
   Badge,
+  Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -10,7 +11,9 @@ import {
   Select,
   VStack,
 } from "@chakra-ui/react";
+import { SunIcon } from "@chakra-ui/icons";
 import { SubmitHandler, useForm } from "react-hook-form";
+import Geocode from "react-geocode";
 
 function App() {
   //バリデーション（react-hook-form）
@@ -20,10 +23,15 @@ function App() {
     formState: { errors },
   } = useForm<any>();
 
+  const [date, setDate] = useState<any>([]);
+  const [dateValue, setDateValue] = useState<any>([]);
   const [prefectures, setPrefectures] = useState<any>([]);
   const [prefCodes, setPrefCodes] = useState<any>([]);
   const [prefecture, setPrefecture] = useState<string>();
   const [region, setRegion] = useState<string>();
+  //const [lat, setLat] = useState<number | null>(null);
+  //const [lng, setLng] = useState<number | null>(null);
+  const [weather, setWeather] = useState<any>([]);
 
   useEffect(() => {
     //RESAS-APIより都道府県データを取得
@@ -37,6 +45,52 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
+
+    //表示終了日
+    const end = 5;
+    //日本の曜日
+    const week = ["日", "月", "火", "水", "木", "金", "土"];
+
+    const dates: any = [];
+    const dateValues: any = [];
+    if (end !== undefined) {
+      for (let i = 0; i < end; i++) {
+        //取得する日付の値を設定
+        let param = Date.now() + i * 86400000;
+        //値から日付を取得
+        let date = new Date(param);
+
+        //dateから年を取得
+        let y = date.getFullYear();
+        //dateから月を取得
+        let m = String(date.getMonth() + 1);
+        //dateから日を取得
+        let d = String(date.getDate());
+        //dateから曜日を取得
+        let w = date.getDay();
+
+        //月を2桁に揃える
+        if (Number(m) < 10) {
+          m = "0" + m;
+        }
+        //日を2桁に揃える
+        if (Number(d) < 10) {
+          d = "0" + d;
+        }
+
+        //テキストの出力形式
+        let textFormat = y + "年" + m + "月" + d + "日" + "(" + week[w] + ")";
+        //値の出力形式
+        let valueFormat = y + "-" + m + "-" + d;
+        dates.push(textFormat);
+        dateValues.push(valueFormat);
+      }
+      setDate(dates);
+      setDateValue(dateValues);
+    }
+    console.log(date);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onChangePrefecture: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
@@ -55,31 +109,69 @@ function App() {
       });
   };
 
-  const onChangeCity: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    setRegion(prefecture + e.target.value);
-  };
-
-  const onClickWeather = () => {
-    const API_KEY = "c176d7dd8a0848fd991bd65e58b2d290";
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=33.5573&lon=130.1955&appid=${API_KEY}&lang=ja&units=metric`
-      )
-      .then((results) => {
-        console.log(results.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const onChangeCity: React.ChangeEventHandler<HTMLSelectElement> = async (e) => {
+    //都道府県＋市区町村で結合した文字列をもとに緯度・経度を取得し天気情報をセット
+    Geocode.setApiKey(process.env.REACT_APP_GOOGLEMAP_KEY ?? "");
+    await Geocode.fromAddress(prefecture + e.target.value).then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        axios
+          .get(
+            //`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_WHEATHER_KEY}&lang=ja&units=metric`
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${process.env.REACT_APP_WHEATHER_KEY}&lang=ja&units=metric`
+          )
+          .then((results) => {
+            setWeather(results.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   };
 
   const onSubmit: SubmitHandler<any> = async (data) => {
     console.log(data);
+    console.log(weather.list);
+    const filterWeather = Object.values(weather.list).filter((item: any) => {
+      return item.dt_txt?.indexOf(data.date) >= 0;
+    });
+    console.log(filterWeather);
   };
 
   return (
     <VStack>
+      {/* 日時選択欄 */}
       <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl mb={4} isInvalid={errors.date ? true : false}>
+          <HStack mb={3}>
+            <Badge variant="solid" colorScheme="red">
+              必須
+            </Badge>
+            <FormLabel fontWeight="bold" color="blue.400">
+              日付
+            </FormLabel>
+          </HStack>
+          <Select
+            id="date"
+            placeholder="日付を選択"
+            w="100%"
+            {...register("date", {
+              required: "日付を選択してください",
+            })}
+          >
+            <option value={dateValue[0]}>{date[0]}</option>
+            <option value={dateValue[1]}>{date[1]}</option>
+            <option value={dateValue[2]}>{date[2]}</option>
+            <option value={dateValue[3]}>{date[3]}</option>
+            <option value={dateValue[4]}>{date[4]}</option>
+          </Select>
+          <FormErrorMessage>{errors.date?.message?.toString()}</FormErrorMessage>
+        </FormControl>
+
         {/* 都道府県選択欄 */}
         <FormControl mb={4} isInvalid={errors.prefecture ? true : false}>
           <HStack mb={3}>
@@ -142,8 +234,9 @@ function App() {
           </Select>
           <FormErrorMessage>{errors.city?.message?.toString()}</FormErrorMessage>
         </FormControl>
-        <button type="submit">表示</button>
-        <button onClick={onClickWeather}>表示</button>
+        <Button type="submit" leftIcon={<SunIcon />} colorScheme="blue" variant="solid">
+          天気チェック
+        </Button>
       </form>
     </VStack>
   );
